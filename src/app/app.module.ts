8df63cbec0fa54f99411616214e026774bcfe449
illud <illudharpyja@gmail.com>
2020-02-21 21:33:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BlackboardComponent } from './blackboard/blackboard.component';
+import  { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'blackboard', component: BlackboardComponent },
+  { path: 'users', component: UsersComponent },
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BlackboardComponent,
+    UsersComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      {useHash: true} // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
